bb8ac6d2e91ddbd9e109dde33af903e02b550891
msi1354 <86691276+msi1354@users.noreply.github.com>
2021-08-11 10:09:41 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    control.reset()
+})
 let time = 0
 basic.showIcon(IconNames.Happy)
 radio.setGroup(2)
 basic.forever(function () {
     if (input.magneticForce(Dimension.Strength) > 250) {
         radio.sendNumber(1)
-        time = 60
+        time = 30
         while (time > 0 && input.magneticForce(Dimension.Strength) > 250) {
             time += 0 - 1
             basic.showNumber(time)
-            basic.pause(500)
         }
-        basic.showLeds(`
-            # . # . #
-            # . # . #
-            # . # . #
-            . . . . .
-            # . # . #
-            `)
+        while (time == 0) {
+            radio.sendNumber(2)
+            basic.showLeds(`
+                # . # . #
+                # . # . #
+                # . # . #
+                . . . . .
+                # . # . #
+                `)
+        }
     } else {
         basic.showLeds(`
             . . . . .
